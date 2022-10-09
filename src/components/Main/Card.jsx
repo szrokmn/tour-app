10e6a26554224eec0c62a5b68489534ccec9468e
Sezer Ökmen <szrokmn34@gmail.com>
2022-10-09 17:52:19 +0300

    tour-app finished

--- src/components/Main/Card.jsx
+++ src/components/Main/Card.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 
 const Card = (data) => {
+  const {id, title, desc, image} = data
   return (
     <div className='cards'>
-      <div className='title'><h1>{data.title}</h1></div>
-      <img src={data.image} alt="" />
-      <div className='card-over'><p>{data.desc}</p></div>
+      <div className='title'><h1>{title}</h1></div>
+      <img src={image} alt="" />
+      <div className='card-over'><p>{desc}</p></div>
     </div>
   )
 }
