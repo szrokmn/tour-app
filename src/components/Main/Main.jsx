f6689445f107aebc4f1235484825f5e4dc929d13
Sezer Ökmen <szrokmn34@gmail.com>
2022-10-09 17:30:41 +0300

    make data.map() on main Main.jsx

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {data} from '../../assests/data'
+
+const Main = () => {
+    // console.log(data);    
+  return (
+    <div className='card-container'>
+      {data.map((item) => co)}
+    </div>
+  )
+}
+export default Main
